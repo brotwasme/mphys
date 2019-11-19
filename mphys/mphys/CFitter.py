82f640886fa5e3af2d69c0f54545fa4c2e078cbf
brotwasme <brotwasme@gmail.com>
2019-11-19 11:53:03 +0000

    expanded dynesty use

---
+++ mphys/mphys/CFitter.py
@@ -0,0 +1,57 @@
+
+# from refnx.analysis import Objective
+from refnx._lib import flatten
+from refnx._lib import unique as f_unique
+import numpy as np
+
+# class ObjectiveOverlay(Objective):
+#     def __init__(self, model, data, lnsigma=None, use_weights=True,
+#                  transform=None, logp_extra=None, name=None):
+#         super().__init__(model, data, lnsigma=None, use_weights=True,
+#                  transform=None, logp_extra=None, name=None):
+
+
+#     def ptform():
+#         pass
+
+
+class CFitter:
+    def __init__(self, objective):
+        self.objective=objective
+        self.pValues=None
+        self.upperBounds=None
+        self.lowerBounds=None
+        self.nDimensions=None
+        self.bounds()
+        self.nDim()
+
+    def priorTransform(self,u):
+        return u*(self.upperBounds-self.lowerBounds) + self.lowerBounds
+
+    def bounds(self):
+        bounds = list(f_unique( (p.bounds.lb, p.bounds.ub) for p
+                     in flatten(self.objective.parameters) if p.vary ))
+        self.lowerBounds = np.array(list(bound[0] for bound in bounds))
+        self.upperBounds = np.array(list(bound[1] for bound in bounds))
+
+    def nDim(self):
+        if self.nDimensions is None:
+            self.nDimensions = len(list(f_unique(p for p
+                     in flatten(self.objective.parameters) if p.vary )))
+        return self.nDimensions
+
+#     @property
+#     def pValues(self):
+#         return self.pValues
+
+#     @pValues.setter
+#     def pVals(self, pValues):
+#         self.pValues=pValues
+#         self.objective.setp(pValues)
+
+    def logl(self, pvalues):
+#         self.pVals(pvalues)
+        return self.objective.logl(pvalues)#self.pValues)
+
+    def fit(self):
+        pass
