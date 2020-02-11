2bed45adee84263117079099c87d676da3ebeb49
brotwasme <brotwasme@gmail.com>
2020-02-11 11:24:05 +0000

    current form

---
+++ mphys/lipidBilayerAsGiven/LipidLeaflet_WaterPer_builtOn3.py
@@ -0,0 +1,167 @@
+import numpy as np
+from refnx.reflect import Component, SLD, ReflectModel, Structure, LipidLeaflet
+from refnx.analysis import possibly_create_parameter, Parameters, Parameter
+
+class LipidLeafletWithWaterPer(LipidLeaflet):
+    def __init__(self, apm, b_heads, vm_heads, thickness_heads,
+                 b_tails, vm_tails, thickness_tails, rough_head_tail,
+                 rough_preceding_mono,
+
+                 water_vm, waters_per_head, waters_per_tail,
+                
+                 head_solvent=None, tail_solvent=None,
+                 reverse_monolayer=False, name=''):
+
+        super(LipidLeafletWithWaterPer, self).__init__(apm, b_heads, vm_heads, thickness_heads,
+                 b_tails, vm_tails, thickness_tails, rough_head_tail,
+                 rough_preceding_mono, head_solvent, tail_solvent,
+                 reverse_monolayer, name)
+
+        self.water_vm = possibly_create_parameter(
+                water_vm,
+                name='%s - water_vm' % name)
+
+        self.waters_per_head = possibly_create_parameter(
+                waters_per_head,
+                name='%s - waters_per_head' % name)
+        
+        self.waters_per_tail = possibly_create_parameter(
+                waters_per_tail,
+                name='%s - waters_per_tail' % name)
+        
+        bo = 0.5804e-4     #Oxygen
+        bh = -0.3741e-4    #Hydrogen
+        bd = 0.6671e-4     #Deuterium
+        D2O = (2*bd) + (1*bo)
+        H2O = (2*bh) + (1*bo)
+        self.D2O = float(D2O)
+        self.H2O = float(H2O)
+
+    def d2o_mol_fraction_calc(self, r, i):
+        r,i = r.value, i.value
+#         print(self.name,"d2o_mol_fraction_calc", r, i)
+        return (1/self.D2O-self.H2O)*((r*27.64)-self.H2O), (1/self.D2O-self.H2O)*((i*27.64)-self.H2O)
+    
+    def d2o_mol_fraction_head(self):
+        return self.d2o_mol_fraction_calc(self.head_solvent.real, self.head_solvent.imag)
+
+    def d2o_mol_fraction_tail(self):
+        return self.d2o_mol_fraction_calc(self.tail_solvent.real, self.tail_solvent.imag)
+
+    def wMol_calc(self, r, i):
+#         print(self.name,"wMol_calc",r,i)
+        return (r * self.D2O) + ((1-r)*self.H2O), (i * self.D2O) + ((1-i)*self.H2O)
+
+    def wMol_head(self):
+        d2o_mol_fraction_head_r, d2o_mol_fraction_head_i = self.d2o_mol_fraction_head()
+        return self.wMol_calc(d2o_mol_fraction_head_r, d2o_mol_fraction_head_i)
+
+    def wMol_tail(self):
+        d2o_mol_fraction_tail_r, d2o_mol_fraction_tail_i = self.d2o_mol_fraction_tail()
+        return self.wMol_calc(d2o_mol_fraction_tail_r, d2o_mol_fraction_tail_i)
+
+    def calc_solvent_true(self, r, i):
+#         print(self.name,"calc_solvent_true",r,i)
+        return complex(r*self.waters_per_head.value/self.vm_head(), i*self.waters_per_head.value/self.vm_head())
+
+    def head_solvent_true(self):
+        wMol_r, wMol_i = self.wMol_head()
+#         print("wMol_r, wMol_i:", wMol_r, wMol_i)
+        return self.calc_solvent_true(wMol_r, wMol_i)
+
+    def tail_solvent_true(self):
+        wMol_tail_r, wMol_tail_i = self.wMol_tail()
+#         print("wMol_tail_r, wMol_tail_i:", wMol_tail_r, wMol_tail_i)
+        return complex(wMol_tail_r*self.waters_per_tail.value/self.vm_tail(), wMol_tail_i*self.waters_per_tail.value/self.vm_tail())
+
+    def vm_head(self):
+        return self.vm_heads.value + self.water_vm.value * self.waters_per_head.value
+
+    def vm_tail(self):
+        return self.vm_tails.value + self.water_vm.value * self.waters_per_tail.value
+
+    def sld_r(self):
+        head_sld = float(self.b_heads_real) / float(self.vm_head()) * 1.e6
+        tail_sld = float(self.b_tails_real) / float(self.vm_tail()) * 1.e6
+        return head_sld, tail_sld
+
+    def sld_i(self):
+        head_sld = float(self.b_heads_imag) / float(self.vm_head()) * 1.e6
+        tail_sld = float(self.b_tails_imag) / float(self.vm_tail()) * 1.e6
+        return head_sld, tail_sld
+
+    def slabs(self, structure=None):
+        """
+        Slab representation of monolayer, as an array
+
+        Parameters
+        ----------
+        structure : refnx.reflect.Structure
+            The Structure hosting this Component
+        """
+        layers = np.zeros((2, 5))
+
+        # thicknesses
+        layers[0, 0] = float(self.thickness_heads)
+        layers[1, 0] = float(self.thickness_tails)
+
+        # real and imag SLD's
+        head_sld_r, tail_sld_r = self.sld_r()
+        head_sld_i, tail_sld_i = self.sld_i()
+        layers[0, 1] = head_sld_r
+        layers[0, 2] = head_sld_i
+
+        layers[1, 1] = tail_sld_r
+        layers[1, 2] = tail_sld_i
+
+        # roughnesses
+        layers[0, 3] = float(self.rough_preceding_mono)
+        layers[1, 3] = float(self.rough_head_tail)
+
+        # volume fractions
+        # head region
+        volfrac = self.vm_head() / (self.apm.value *
+                                         self.thickness_heads.value)
+        layers[0, 4] = 1 - volfrac
+        if self.head_solvent is not None:
+            # we do the solvation here, not in Structure.slabs
+            layers[0] = Structure.overall_sld(layers[0], self.head_solvent_true())
+            layers[0, 4] = 0
+
+        # tail region
+        volfrac = self.vm_tail() / (self.apm.value *
+                                         self.thickness_tails.value)
+
+        layers[1, 4] = 1 - volfrac
+        if self.tail_solvent is not None:
+            # we do the solvation here, not in Structure.slabs
+            layers[1] = Structure.overall_sld(layers[1], self.tail_solvent_true())
+            layers[1, 4] = 0
+
+        if self.reverse_monolayer:
+            layers = np.flipud(layers)
+            layers[:, 3] = layers[::-1, 3]
+#         print("layers",layers)
+        return layers
+
+    @property
+    def parameters(self):
+        p = super(LipidLeafletWithWaterPer, self).parameters
+        p.extend([
+            self.water_vm, self.waters_per_head, self.waters_per_tail])
+        return p
+
+    def logp(self):
+        # penalise unphysical volume fractions.
+        volfrac_h = self.vm_head() / (self.apm.value *
+                                    self.thickness_heads.value)
+
+        # tail region
+        volfrac_t = self.vm_tail() / (self.apm.value *
+                                    self.thickness_tails.value)
+
+        if volfrac_h > 1 or volfrac_t > 1:
+            return -np.inf
+
+        return 0
+
---
+++ mphys/protien/LipidLeafletWithProtein_builtOn2.py
@@ -0,0 +1,123 @@
+import numpy as np
+from refnx.reflect import Component, SLD, ReflectModel, Structure, LipidLeaflet
+from refnx.analysis import possibly_create_parameter, Parameters, Parameter
+from lipidBilayerAsGiven.LipidLeaflet_WaterPer_builtOn3 import LipidLeafletWithWaterPer
+
+class LipidLeafletWithProtien(LipidLeafletWithWaterPer):
+    def __init__(self, apm, b_heads, vm_heads, thickness_heads,
+                 b_tails, vm_tails, thickness_tails, rough_head_tail,
+                 rough_preceding_mono,
+
+                 water_vm, waters_per_head, waters_per_tail,
+                 vm_mscl, PLRatio,
+                
+                 head_solvent=None, tail_solvent=None,
+                 reverse_monolayer=False, name=''):
+
+        super(LipidLeafletWithProtien, self).__init__( apm,
+                 b_heads, vm_heads, thickness_heads,
+                 b_tails, vm_tails, thickness_tails, rough_head_tail,
+                 rough_preceding_mono,
+
+                 water_vm, waters_per_head, waters_per_tail,
+                
+                 head_solvent, tail_solvent,
+                 reverse_monolayer, name)
+
+#         if isinstance(b_mscl, complex):
+#             self.b_mscl_real = possibly_create_parameter(
+#                 b_mscl.real,
+#                 name='%s - b_mscl_real' % name)
+#             self.b_mscl_imag = possibly_create_parameter(
+#                 b_mscl.imag,
+#                 name='%s - b_mscl_imag' % name)
+#         elif isinstance(b_mscl, SLD):
+#             self.b_mscl_real = b_mscl.real
+#             self.b_mscl_imag = b_mscl.imag
+#         else:
+#             self.b_mscl_real = possibly_create_parameter(
+#                 b_mscl,
+#                 name='%s - b_mscl_real' % name)
+#             self.b_mscl_imag = possibly_create_parameter(
+#                 0,
+#                 name='%s - b_mscl_imag' % name)
+        
+        self.vm_mscl = possibly_create_parameter(
+                vm_mscl,
+                name='%s - vm_mscl, protien volume' % name)
+        
+        self.PLRatio = possibly_create_parameter(
+                PLRatio,
+                name='%s - PLRatio, protien lipid ratio' % name)
+
+    def b_mscl_head(self):
+        bc = 0.6646e-4  #Carbon
+        bo = 0.5804e-4  #Oxygen
+        bh = -0.3739e-4 #Hydrogen
+        #bp = 0.513e-4   #Phosphorus
+        bn = 0.936e-4   #Nitrogen
+        bd = 0.6671e-4  #Deuterium
+        bs = 2.847e-4   #Sulphur
+        mol_frac,i = self.d2o_mol_fraction_head()
+        return float(2745*bc + 675*bn + 641*bo + 25*bs + 
+                    (4374.5-822.5*0.9)*bh +
+                    mol_frac*822.5*0.9*bd + 
+                    (1-mol_frac)*822.5*0.9*bh)
+
+    def b_mscl_tail(self):
+        bc = 0.6646e-4  #Carbon
+        bo = 0.5804e-4  #Oxygen
+        bh = -0.3739e-4 #Hydrogen
+        #bp = 0.513e-4   #Phosphorus
+        bn = 0.936e-4   #Nitrogen
+        bd = 0.6671e-4  #Deuterium
+        bs = 2.847e-4   #Sulphur
+        mol_frac,i = self.d2o_mol_fraction_tail()
+        return float(2745*bc + 675*bn + 641*bo + 25*bs + 
+                    (4374.5-822.5*0.9)*bh +
+                    mol_frac*822.5*0.9*bd + 
+                    (1-mol_frac)*822.5*0.9*bh)
+
+
+    def sld_r(self):
+        lipid_head_sld = float(self.b_heads_real) / float(self.vm_head()) * 1.e6
+        lipid_tail_sld = float(self.b_tails_real) / float(self.vm_tail()) * 1.e6
+        
+        mscl_h_sld_r = self.b_mscl_head()/float(self.vm_mscl.value)# * 1.e6
+        mscl_t_sld_r = self.b_mscl_tail()/float(self.vm_mscl.value)# * 1.e6
+
+        mscl_head_sld = mscl_h_sld_r*(float(self.thickness_heads)/
+                                  (2*(float(self.thickness_heads)+float(self.thickness_tails))))
+        mscl_tail_sld = mscl_t_sld_r*(float(self.thickness_heads)/
+                                  (2*(float(self.thickness_heads)+float(self.thickness_tails))))
+
+        head_sld = (self.PLRatio*lipid_head_sld) + (1-self.PLRatio)*mscl_head_sld
+        tail_sld = (self.PLRatio*lipid_tail_sld) + (1-self.PLRatio)*mscl_tail_sld
+        return float(head_sld), float(tail_sld)
+
+    def sld_i(self):
+        lipid_head_sld = float(self.b_heads_imag) / float(self.vm_head()) * 1.e6
+        lipid_tail_sld = float(self.b_tails_imag) / float(self.vm_tail()) * 1.e6
+
+# #         b_mscl_head_r, b_mscl_head_i = self.b_mscl_head()
+# #         b_mscl_tail_r, b_mscl_tail_i = self.b_mscl_tail()
+        
+#         mscl_h_sld_i = float(b_mscl_head_i)/float(self.vm_mscl.value)# * 1.e6
+#         mscl_t_sld_i = float(b_mscl_tail_i)/float(self.vm_mscl.value)# * 1.e6
+
+#         mscl_head_sld = mscl_h_sld_i*(float(self.thickness_heads)/
+#                                   (2*(float(self.thickness_heads)+float(self.thickness_tails))))
+#         mscl_tail_sld = mscl_t_sld_i*(float(self.thickness_heads)/
+#                                   (2*(float(self.thickness_heads)+float(self.thickness_tails))))
+
+        #head_sld = lipid_head_sld#(self.PLRatio*lipid_head_sld) + (1-self.PLRatio)*0#mscl_head_sld
+        #tail_sld = lipid_tail_sld#`(self.PLRatio*lipid_tail_sld) + (1-self.PLRatio)*0#mscl_tail_sld
+        return lipid_head_sld, lipid_tail_sld
+
+
+    @property
+    def parameters(self):
+        p = super(LipidLeafletWithProtien, self).parameters
+        p.extend([self.vm_mscl,
+                    self.PLRatio])
+        return p
